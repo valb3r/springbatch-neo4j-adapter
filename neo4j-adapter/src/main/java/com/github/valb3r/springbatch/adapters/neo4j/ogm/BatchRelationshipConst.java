0bc2c088c4ec47c9a420d62fb153bf3c1ed0d0f3
Valentyn Berezin <valb3r.ua@gmail.com>
2020-02-02 14:01:26 +0200

    Initial adapter version

---
+++ neo4j-adapter/src/main/java/com/github/valb3r/springbatch/adapters/neo4j/ogm/BatchRelationshipConst.java
@@ -0,0 +1,9 @@
+package com.github.valb3r.springbatch.adapters.neo4j.ogm;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BatchRelationshipConst {
+
+    public static final String PARENT = "PARENT";
+}
